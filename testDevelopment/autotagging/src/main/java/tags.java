850b56c5081b3127a40e6b2c712429d30e7ac356
“watermelonsz” <shuyanzhou0218@gmail.com>
2019-06-19 08:19:27 -0700

    add code to read in sample csv data file and implement aggregation count by tag and save it into redis table

--- testDevelopment/autotagging/src/main/java/tags.java
+++ testDevelopment/autotagging/src/main/java/tags.java
@@ -3,8 +3,7 @@
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-
-
+import org.apache.spark.sql.SaveMode;
 
 public class tags {
     public static void main(String[] args) {
@@ -16,6 +15,27 @@ public class tags {
                 .config("spark.redis.host", "10.0.0.12")
                 .config("spark.redis.port", "6379")
                 .getOrCreate();
+             //config to read int csv file
+        Dataset<Row> data = spark.read()
+                .option("multiLine", true)
+                .option("header", true)
+                .option("escape", "\"")
+                .csv(inputPath);
+        //apply split and explode function to field 'tags' and flat one single column "tags" into multiple rows of field 'tag'
+        Dataset<Row> tags = data.select(
+                org.apache.spark.sql.functions.explode(org.apache.spark.sql.functions.split(data.col("tags"), "\\|"))
+                        .as("tag")
+        );
+        //did aggregation count based on 'tag' field
+        Dataset<Row> counts = tags.groupBy("tag").count();
+        counts.orderBy(counts.col("count").desc());
+        //write (key, value) pair into redis table called 'tag'
+        counts.write()
+                .format("org.apache.spark.sql.redis")
+                .option("table", "tag")
+                .option("key.column", "tag")
+                .mode(SaveMode.Overwrite)
+                .save();
         
         spark.stop();
     }
