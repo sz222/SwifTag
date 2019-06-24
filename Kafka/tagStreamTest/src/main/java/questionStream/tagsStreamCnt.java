8cab933f4f8f21b118134e41df0eefb3c1706615
“watermelonsz” <shuyanzhou0218@gmail.com>
2019-06-23 20:39:25 -0700

    finish first version kafka stream code to provide tag recommendation to questionse

--- Kafka/tagStreamTest/src/main/java/questionStream/tagsStreamCnt.java
+++ Kafka/tagStreamTest/src/main/java/questionStream/tagsStreamCnt.java
@@ -1,8 +1,9 @@
-package questionStream;
 /*
 This application file processes question stream data and interacts with Redis and
 provide tags recommendation for incoming questions
  */
+package questionStream;
+
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.KStream;
@@ -33,12 +34,8 @@ public class tagsStreamCnt {
         source.mapValues(value -> {
             Gson gson = new GsonBuilder().create();
             Question question = gson.fromJson(value, Question.class);
-//            System.out.println(question.getTitle());
-//            String[] tags = getTagsRecommended(question.getTitle(), jedis);
-            getTagsRecommended(question.getTitle(), jedis);
-//            question.setTags(tags);
-//            question.setTags(question.getTitle().split(" "));
-//            System.out.println(gson.toJson(question));
+            Set<String> tags = getTagsRecommended(question.getTitle(), jedis);
+            question.setTags(tags.toArray(new String[0]));
             return gson.toJson(question);
 
         }).to("streams-tags-output");
@@ -65,7 +62,7 @@ public class tagsStreamCnt {
             System.exit(0);
     }
 
-    public static void getTagsRecommended(String title, Jedis jedis) {
+    public static Set<String> getTagsRecommended(String title, Jedis jedis) {
 //        if (!stopWords.contains(word))
 //        String[] tagsList = new String[3];
 
@@ -74,24 +71,70 @@ public class tagsStreamCnt {
         //1) "collect_list(tagTuple)"
         //2) "WrappedArray([scheme,1], [stream,1])"
         Map<String, Integer> tagMap = new HashMap<>(); //create a map with key:tag name value: tag count
-        
+
         String[] keyWords = title.split(" ");
         for (String word : keyWords) {
             if (!stopWords.contains(word.toLowerCase())) {
                 if (jedis.hgetAll("questionTag:" + word.toLowerCase()) != null) {
                     for(String key : jedis.hgetAll("questionTag:" + word.toLowerCase()).keySet()) {
-                        System.out.println("*****" + key);
+                        String tagsCnt = jedis.hgetAll("questionTag:" + word.toLowerCase()).get(key);
+
+                        //example:*****WrappedArray([scheme,1], [stream,1])
+                        //manipulate string result to get a list of array[tag, cnt]
+                        //step 1: delete "WrappedArray(" and the last ")"
+                        String result = tagsCnt.substring(13, tagsCnt.length() - 1);
+                        //Step 2. split the result into array
+                        String[] resultInd = result.split(", ");
+                        for (String element : resultInd) {
+                            //Step 3: for each element in the array, manipulate each string and
+                            // extract key: tag and value count and store them into the info map
+                            String[] keyAndVal = element.substring(1, element.length() - 1).split(",");
+                            String keyTag = keyAndVal[0];
+                            Integer tagCnt = Integer.valueOf(keyAndVal[1]);
+                            if (tagMap.containsKey(keyTag)) {
+                                Integer origVal = tagMap.get(keyTag);
+                                tagMap.put(keyTag, origVal + tagCnt);
+                            } else {
+                                tagMap.put(keyTag, tagCnt);
+                            }
+                        }
+
                     };
-                    //iterate through the value(array of [tag, count] tuple of keyword in Redis
-//                    String[] tagsCount = jedis.hgetAll("questionTag:" + word.toLowerCase());
-//
                 }
             }
         }
-//        return tagsList;
+
+        ValueComparator bvc = new ValueComparator(tagMap);
+        TreeMap<String, Integer> sorted_map = new TreeMap<String, Integer>(bvc);
+        sorted_map.putAll(tagMap);
+
+        TreeMap<String, Integer> topTags = sorted_map
+                .entrySet()
+                .stream()
+                .limit(3)
+                .collect(TreeMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
+        return topTags.keySet();
     }
 
     public static Set<String> stopWords = new HashSet<String>(Arrays.asList(
             "and", "an", "i", "not", "is", "are", "?",
             ".", "who", "in", "to", "how", "a", "the", "using" ));
+
+    static class ValueComparator implements Comparator<String> {
+        Map<String, Integer> base;
+
+        public ValueComparator(Map<String, Integer> base) {
+            this.base = base;
+        }
+
+        // Note: this comparator imposes orderings that are inconsistent with
+        // equals.
+        public int compare(String a, String b) {
+            if (base.get(a) >= base.get(b)) {
+                return -1;
+            } else {
+                return 1;
+            } // returning 0 would merge keys
+        }
+    }
 }
